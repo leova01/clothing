a2bc299d7b6c2389abfbf859e61067190eaae461
leonel <leovalcarcel01@gmail.com>
2026-01-02 15:15:57 -0400

    test

--- app/(pages)/designTool/page.tsx
+++ app/(pages)/designTool/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <div className="flex h-screen items-stretch bg-zinc-50 font-sans">
             <div className="flex w-full  flex-col md:flex-row">
                 <div className="w-full  md:w-1/2 bg-blue-900 p-8">
-                    Columna 1
+                    Columna 1 - test
                 </div>
                 <div className="w-full  md:w-1/2 bg-green-900 ">Columna 2</div>
             </div>
